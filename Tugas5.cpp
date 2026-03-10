a15f04f0cef95a38576675030d1f7f6da7762365
andilutfi944-afk <andilutfi944@gmail.com>
2026-03-10 10:05:33 +0700

    Menambahkan if untuk untuk mengecek nilai berat bandan kurang,berat badan normal,berat badan kelebihan,dan obesitas

--- Tugas5.cpp
+++ Tugas5.cpp
@@ -18,6 +18,14 @@ double hitungBMI(double berat, double tinggi) {
 }
 
 // Fungsi untuk melihat kondisi berat badan
-string cekKondisi(double bmi){
-    
+string cekKondisi(double bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } else if (bmi < 25) {
+        return "Berat Badan Normal";
+    } else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } else {
+        return "Obesitas";
+    }
 }
